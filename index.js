93c0416aca2b483a684340715fb805190a7f9607
Gibran Dimasagung <gibrandev@gmail.com>
2025-11-17 22:13:04 +0700

    Add cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 3000
 
+app.use(cors())
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
